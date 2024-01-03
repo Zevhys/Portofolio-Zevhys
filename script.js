e08b308bf3f11729d0de9577161bb0923e1ab523
Zev <rakha2728@gmail.com>
2024-01-03 22:29:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -19,12 +19,45 @@ function onFormSubmit(e) {
   }
 }
 
+// Spread Sheet
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycby7JkuVn04VE-Dla5Jrekzzi6MMulPmqCqBHzIXInkT_u9MRjfPPkmbqhC81Gu7SFUuXA/exec";
+const form = document.forms["zevhys-contact-form"];
+const btn_submit = $("#btn-submit")[0];
+const submitted_spinner = $("#submitted-spinner")[0];
+const submitted_text = $("#submitted-text")[0];
+
+console.log(btn_submit);
+
+function btn_submit_restore() {
+  btn_submit.disabled = false;
+  submitted_text.textContent = "Submit";
+  submitted_spinner.classList.toggle("hidden");
+}
+
+form.addEventListener("submit", (e) => {
+  btn_submit.disabled = true;
+  submitted_text.textContent = "Submitting";
+  submitted_spinner.classList.toggle("hidden");
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then(function (response) {
+      btn_submit_restore();
+      alert("Form Submitted");
+    })
+    .catch(function (error) {
+      btn_submit_restore();
+      alert(`An error occured: \n ${error}`);
+    });
+});
+
 // Function to handle form submission
+const email = $("#inp_email");
+const subject = $("#inp_subject");
+const message = $("#inp_message");
+
 function onFormSubmit(e) {
   e.preventDefault();
-  const email = $("#inp_email");
-  const subject = $("#inp_subject");
-  const message = $("#inp_message");
 
   if (!email.val()) {
     alert("Email is required");
@@ -33,10 +66,9 @@ function onFormSubmit(e) {
   } else if (!message.val()) {
     alert("Message is required");
   } else {
-    alert("Form Submitted");
-    email.val("");
-    subject.val("");
-    message.val("");
+    // email.val("");
+    // subject.val("");
+    // message.val("");
   }
 }
 
@@ -84,4 +116,8 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
   },
+});
+
+$(function () {
+  $('[data-bs-toggle="tooltip"]').tooltip();
 });
